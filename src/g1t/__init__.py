f9f3329a77a49aa5957073717c50d7bff76e6cef
umepon0626 <ryuichiastrona@gmail.com>
2024-05-26 22:57:00 +0900

    とりあえずgit init

--- src/g1t/__init__.py
+++ src/g1t/__init__.py
@@ -1,7 +1,41 @@
 import click
+from g1t.core.repository import Repository
+from configparser import ConfigParser
+from pathlib import Path
 
-@click.command()
-@click.option("--name", default="World", help="Name to greet.")
-def main(name) -> int:
-    print(f"Hello {name}.")
+
+@click.group()
+def main() -> int:
     return 0
+
+
+@main.command()
+@click.argument("path", default=".")
+def cmd_init(path) -> int:
+    repo = Repository(
+        Path(path).absolute(),
+    )
+    if repo.gitdir.exists():
+        raise Exception(f"Path already exists {repo.gitdir}")
+    else:
+        repo.gitdir.mkdir(parents=True)
+
+    (repo.gitdir / "objects").mkdir(exist_ok=True)
+    (repo.gitdir / "branches").mkdir(exist_ok=True)
+    (repo.gitdir / "refs" / "tags").mkdir(exist_ok=True, parents=True)
+    (repo.gitdir / "refs" / "heads").mkdir(exist_ok=True, parents=True)
+
+    with (repo.gitdir / "config").open("w") as f:
+        config = repo_default_config()
+        config.write(f)
+    return repo
+
+
+def repo_default_config():
+    ret = ConfigParser()
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
---
+++ src/g1t/core/repository.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from configparser import ConfigParser
+
+
+class Repository:
+    def __init__(self, path: Path, check_dir_exist: bool = False) -> None:
+        self.worktree = path
+        self.gitdir = path / ".g1t"
+
+        if check_dir_exist and not self.gitdir.is_dir():
+            raise Exception(f"Not a git repository {path}")
+
+        config_parser = ConfigParser()
+        config_file = self.gitdir / "config"
+        if config_parser and config_file.exists():
+            config_parser.read(config_file)
+            if check_dir_exist:
+                raise Exception(f"Config file missing {config_file}")
