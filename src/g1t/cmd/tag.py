685c9bb82ba92479c422a412559936aaf7ba0bb0
umepon0626 <ryuichiastrona@gmail.com>
2024-06-29 12:00:02 +0900

    fix tag cmd

--- src/g1t/cmd/tag.py
+++ src/g1t/cmd/tag.py
@@ -5,8 +5,8 @@ from g1t.cmd.show_ref import show_ref
 
 def cmd_tag(obj: str, tag_name: str | None = None, store_object: bool = False) -> None:
     repo = find_repository()
-    obj = read_object(repo, find_object(repo, obj))
     if tag_name is not None:
+        obj = read_object(repo, find_object(repo, obj))
         create_tag(repo, tag_name, obj, store_object)
     refs = list_ref(repo)
     show_ref(refs, with_hash=False)
